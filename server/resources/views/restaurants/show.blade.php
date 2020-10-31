07d7d2c5c7eece5d6001134b83107148abb3bc6d
ToshiyaKonno <tk.ichinoseki@gmail.com>
2020-10-31 11:33:50 +0900

    レイアウト簡略化

--- server/resources/views/restaurants/show.blade.php
+++ server/resources/views/restaurants/show.blade.php
@@ -1,13 +1,10 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>めしナビ - 詳細画面</title>
-</head>
-<body>
-    <table>
+@extends('layouts.app')
+{{-- app.baladeから継承 --}}
+@section('title', '詳細画面')
+
+@section('content')
+            <table class="table-bordered mb-5 mt-3">
+            <colgroup span="1" style="width:200px;background-color:#efefef;"></colgroup>
         <tbody>
             <tr>
                 <th>店名</th>
@@ -35,6 +32,7 @@
         </tbody>
     </table>
     {{-- actionタグは関係なくルーティング出来る --}}
-    <a href="{{ action('RestaurantController@index') }}">戻る</a>    
-</body>
-</html>
+    <a href="{{ action('RestaurantController@index') }}">戻る</a>   
+ 
+
+@endsection
